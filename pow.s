eb4a329ea10fe7d8f1de972b55244546dd3c7f04
esmeedevi <twiinapocalyp2e2@gmail.com>
2015-10-05 17:07:09 +0200

    Update pow.s

--- pow.s
+++ pow.s
@@ -1,56 +1,65 @@
 .data
 printstr:	.asciz "Assignment 3: pow\n\nGive me a positive base and an exponent: \n"
-printstr2:	.asciz "/nResult is: %ld/n"
+printstr2:	.asciz "\nResult is: %ld\n"
 inputstr:	.asciz "%ld" 
-inputstr2:	.asciz "%ld" 
+
 
 .global		main
 
 main:
+	pushq	%rbp			#prologue: push rbp onto stack
+	movq	%rsp, %rbp		#then copy stackpointer to rbp	
+
 	movq	$0, %rax		#no vector args
 	movq    $printstr, %rdi		#move printstr into rdi
 	call	printf			#print
-	
-	call 	input
-	call 	loop
-	call	exit
 
-input:
-	pushq	%rbp			#prologue: push rbp onto stack
-	movq	%rsp, %rbp		#then copy stackpointer to rbp		
+	#Scan variables and put them on the stack
 
-	subq	$8, %rsp		#reserve stack space for variable
+
+	subq	$16, %rsp		#reserve stack space for variable
+
 	leaq	-8(%rbp), %rsi		#load address of stack var in rsi
 	movq	$inputstr, %rdi		#load first argument of scanf
+	movq	$0, %rax		#no vector args
 	call	scanf			#scan variable
 
-	subq	$8, %rsp		#reserve stack space for variable
 	leaq	-8(%rbp), %rsi		#load address of stack var in rdx
-	movq	$inputstr2, %rdi	#load first argument of scanf
+	movq	$inputstr, %rdi		#load first argument of scanf
+	movq	$0, %rax		#no vector args
 	call	scanf			#scan variable
-	
-	popq	%rdx			#pop exponent variable into rdx
-	popq	%rsi			#pop base into rsi
 
-	movq 	%rsi, %rax		#copy base into rax
+	#Put variables into registers, preparations for loop subroutine
+	
+	movq	-8(%rbp), %rsi		#move waarde/value of var to rsi
+	movq	-16(%rbp), %rdx		#move waarde/value of var to rdi
+	call 	loop
+	
 	movq	$0, %rcx		#put the value 0 in rcx
 
+	movq	%rax, %rsi		#copy rax into rsi
+	movq	$printstr2, %rdi
+	call	printf
+
+	call	exit
+
+
 loop:
-	cmpq	%rcx, %rdx		#compare rcx with the exponent in rdx	
-	je 	end			#if so, go to end
-	
-	incq	%rcx			#increment rcx by 1
-	mulq	%rsi			#multiply rsi by the value in rax
+	pushq	%rbp
+	movq	%rsp, %rbp
 
-	jmp	loop
+	movq	$1, %rax
+	subq	$8, %rsp
+	movq	%rdi, -8(%rbp)
 
-end:
-	movq	$printstr2, %rdi	#copy printstr2 into rdi
-	movq	%rsi, %rdx		#copy rsi into rdx
-	movq	$0, %rax		#no vector args
-	call	printf			#print
 
-	movq    %rbp, %rsp		#epilogue: move rbp to rap
-	popq	%rbp			#restore base pointer
-
+powend:
+	imulq 	%rsi, %rax
+	decq 	-8(%rbp)
+	cmpb 	$0, -8(%rbp)
+	jg 	powend
+	movq	%rbp, %rsp
+	popq	%rbp
 	ret
+
+
